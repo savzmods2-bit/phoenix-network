9971d5ff3981138c64bf005e9b389819246c020d
savzmods2-bit <savzmods2@gmail.com>
2026-04-22 19:55:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { scramjetPath } from '@mercuryworkshop/scramjet/path';
+import path from 'node:path';
 
+const app = express();
+const server = createServer(app);
+const __dirname = process.cwd();
+
+// Serves the files in your 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serves the Scramjet engine files
+app.use('/scram/', express.static(scramjetPath));
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`PHOENIX ONLINE: Port ${PORT}`);
+});
